e7a0ca20b08aaa8c15f1d011d6c720442c8a0eb0
Sudina Madhavi <sudina.madhavi@capgemini.com>
2025-05-17 21:54:29 +0530

    adding thr resources

---
+++ terraform/main.tf
@@ -0,0 +1,53 @@
+module "vnet" {
+  source = "git::https://github.com/MadhaviSudina/modules-repo.git//modules/vnet?ref=main"
+  # Pass required variables
+  resource_group      = var.rg_name
+  location            = var.loc
+  name                = var.vnet_name
+  vnet_address_spaces = var.add_space
+  subnets             = var.subnets
+}
+
+module "vm" {
+  source = "git::https://github.com/your-org/modules-repo.git//modules/VM?ref=main"
+  # Pass required variables
+  name                   = var.vm_namoved
+  location               = var.loc
+  resource_group         = var.rg_name
+  sku_name               = var.sku
+  os_disk                = var.os_disk
+  source_image_reference = var.source_image_reference
+  linux_configuration    = var.linux_configuration
+  nic_name               = var.nic_name
+  context                = var.context
+  subnet_id              = var.subnet_id
+}
+
+
+
+module "storage" {
+  source = "git::https://github.com/your-org/modules-repo.git//modules/storage?ref=main"
+  # Pass required variables
+  name                            = var.st_name
+  location                        = var.loc
+  resource_group                  = var.rg_name
+  account_tier                    = var.acc_tier
+  account_replication_type        = var.rep_type
+  allow_nested_items_to_be_public = var.allow_nested_items_to_be_public
+}
+
+module "kv" {
+  source = "git::https://github.com/your-org/modules-repo.git//modules/keyvault?ref=main"
+  # Pass required variables
+  name                                   = var.kv_name
+  resource_group                         = var.rg_name
+  sku_name                               = var.kv_sku
+  enable_rbac_authorization              = var.enable_rbac_authorization
+  public_network_access_enabled          = var.public_network_access_enabled
+  soft_delete_retention_days             = var.soft_delete_retention_days
+  purge_protection_enabled               = var.purge_protection_enabled
+  network_acl_default_action             = var.network_acl_default_action
+  network_acl_bypass                     = var.network_acl_bypass
+  network_acl_ip_rules                   = var.network_acl_ip_rules
+  network_acl_virtual_network_subnet_ids = var.network_acl_virtual_network_subnet_ids
+}
