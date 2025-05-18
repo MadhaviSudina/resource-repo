a4ad72249c929fe87f2182d5a8605d759608568e
MadhaviSudina <47038695+MadhaviSudina@users.noreply.github.com>
2025-05-18 12:43:40 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,9 @@
+
+variable "subscription_id" {}
+variable "tenant_id" {}
+variable "client_id" {}
+variable "client_secret" {}
+
 variable "rg_name" {
   type        = string
   description = "The name of the resource group in which to deploy."
