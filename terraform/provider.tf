bf2bd779d16576804903da450c1a8e4264c6d47c
MadhaviSudina <47038695+MadhaviSudina@users.noreply.github.com>
2025-05-19 12:56:41 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,8 +1,8 @@
 provider "azurerm" {
   features {}
-  subscription_id = "835fa347-a12e-4ad3-9dfe-49237ac12057"
-  tenant_id       = "feb8c97f-fcbc-459d-9e83-49a1dbc21b94"
-  client_id       = "d8e0ad20-13bf-4845-8ce3-3e34a59f084a"
-  client_secret   = "Krx8Q~f7brSCXyfI.NwzDF8TKknXwON4bUFL1a95"
+  subscription_id = var.subscription_id
+  tenant_id      = var.tenant_id
+  client_id      = var.client_id
+  client_secret  = var.client_secret
 
 }
